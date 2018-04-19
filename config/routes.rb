5a050e68a04bc1ac2b3c79b9479af09b147a150a
Dimetriu <dima.rashidov@gmail.com>
2018-04-20 01:25:39 +0300

    improve locale

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get '/:locale' => 'home#index'
   root 'home#index'
-
-  devise_for :users
   
+  scope '(:locale)' do
+    devise_for :users
+  end
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 end
