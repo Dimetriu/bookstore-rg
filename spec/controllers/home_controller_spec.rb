e6bf92e863a2da6f83c5d38cfca1cef8a8dd5cbb
Dimetriu <dima.rashidov@gmail.com>
2018-04-20 01:30:01 +0300

    improve home_spec

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe HomeController, type: :controller do
 
   describe "GET #index" do
     it "returns http success" do
-      expect(get: root_url(subdomain: nil)).to route_to(controller: "home", action: "index")
+      expect(get: root_url(subdomain: nil)).to route_to(controller: "home", action: "index", locale: "en")
     end
   end
 
