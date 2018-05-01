3696001bb94ddcbbc905cbd4d7d6119884427551
Dimetriu <dima.rashidov@gmail.com>
2018-05-02 01:13:07 +0300

    improve views

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,26 +3,3 @@
 //= require bootstrap-sprockets
 //= require activestorage
 //= require_tree .
-
-window.fbAsyncInit = function () {
-  FB.init({
-    appId: '1493756687389092',
-    xfbml: true,
-    version: 'v2.12'
-  });
-  FB.AppEvents.logPageView();
-  FB.getLoginStatus(function (response) {
-    statusChangeCallback(response);
-  });
-};
-
-(function (d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) {
-    return;
-  }
-  js = d.createElement(s);
-  js.id = id;
-  js.src = "https://connect.facebook.net/en_US/sdk.js";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
-  
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, 
+  devise :database_authenticatable,
          :registerable,
-         :recoverable, 
-         :rememberable, 
-         :trackable, 
+         :recoverable,
+         :rememberable,
+         :trackable,
          :validatable,
          :confirmable,
          :omniauthable, omniauth_providers: %i[facebook]
@@ -17,7 +17,7 @@ class User < ApplicationRecord
       user.password = Devise.friendly_token[0,20]
       user.first_name = auth.info.first_name
       user.last_name = auth.info.last_name
-      user.image = auth.info.picture
+      user.image = auth.info.image
       # user.skip_confirmation!
     end
   end
