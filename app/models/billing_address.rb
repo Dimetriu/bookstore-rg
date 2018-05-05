e85a0230c59c9ca69b876e12e5835976ee8eab23
Dimetriu <dima.rashidov@gmail.com>
2018-05-06 01:10:37 +0300

    add phone validator

--- app/models/billing_address.rb
+++ app/models/billing_address.rb
@@ -1,3 +1,14 @@
 class BillingAddress < ApplicationRecord
   belongs_to :user, inverse_of: :billing_address
+
+  validates :address,
+            :city,
+            :zip,
+            :country,
+            presence: true
+  validates :first_name, :last_name,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { in: 2..30 }
+  validates :phone, presence: true, phone: true
 end
--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,3 +1,14 @@
 class ShippingAddress < ApplicationRecord
   belongs_to :user, inverse_of: :shipping_address
+
+  validates :address,
+            :city,
+            :zip,
+            :country,
+            presence: true
+  validates :first_name, :last_name,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { in: 2..30 }
+  validates :phone, presence: true, phone: true
 end
