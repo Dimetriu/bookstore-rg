4b410023e5d12e37bddc6742bd7234c710d61ebd
Dimetriu <dima.rashidov@gmail.com>
2018-05-22 17:30:42 +0300

    improve registrations controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def destroy
     resource.soft_delete
     sign_out(resource_name)
-    set_flash_message, :notice, :destroyed
+    set_flash_message :notice, :destroyed
     yield resource if block_given?
     respond_with_navigational(resource) { redirect_to after_signout_path_for(resource_name) }
   end
