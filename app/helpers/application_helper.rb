bafa3c54ce16b358e09bcaf8f8fc9329fc5776a6
Dimetriu <dima.rashidov@gmail.com>
2018-05-25 23:55:07 +0300

    improve tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
-  def categories
-    Category.order(:name).find_each do |c|
-      %li
-        %a
-          = c.name.capitalize
-    end
-  end
+  # def categories
+  #   Category.order(:name).find_each do |c|
+  #     %li
+  #       %a
+  #         = c.name.capitalize
+  #   end
+  # end
 end
--- spec/models/welcome_discount_spec.rb
+++ spec/models/welcome_discount_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe WelcomeDiscount, type: :model do
+RSpec.describe Discounts::WelcomeDiscount, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
