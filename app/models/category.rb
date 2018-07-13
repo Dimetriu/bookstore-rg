30262f2e41f232c3af7444dc12930c68e4e5097f
Dimetriu <dima.rashidov@gmail.com>
2018-07-13 23:08:59 +0300

    add books counter column to categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
-  has_many :books
+  has_many :books, counter_cache: true, dependent: :destroy
 
   validates :name, presence: true, length: { in: 3..50 }, uniqueness: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_11_121933) do
+ActiveRecord::Schema.define(version: 2018_07_13_193019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
@@ -113,6 +113,7 @@ ActiveRecord::Schema.define(version: 2018_07_11_121933) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "books_count", default: 0
     t.index ["name"], name: "index_categories_on_name", unique: true
   end
 
