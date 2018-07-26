8791516f37380d692ec55974be3c25aae7a7a83f
Dimetriu <dima.rashidov@gmail.com>
2018-07-26 16:19:58 +0300

    setup order items association

--- app/models/book.rb
+++ app/models/book.rb
@@ -12,8 +12,8 @@ class Book < ApplicationRecord
 
   has_many :authorships, inverse_of: :book
   has_many :authors, through: :authorships, inverse_of: :books
-  # has_many :order_items, inverse_of: :book
-  # has_many :orders, through: :order_items, inverse_of: :books
+  has_many :order_items, inverse_of: :book
+  has_many :orders, through: :order_items, inverse_of: :books
   has_many :ratings, as: :rateable
 
   mount_uploaders :images, ImageUploader
