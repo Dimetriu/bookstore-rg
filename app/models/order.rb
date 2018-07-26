6c335c95fd2f3316b8f84c74cc2b71c67ed78039
Dimetriu <dima.rashidov@gmail.com>
2018-07-26 16:29:09 +0300

    add subtotal to order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,21 @@ class Order < ApplicationRecord
   belongs_to :user, inverse_of: :orders
   has_many :order_items, inverse_of: :order
   has_many :books, through: :order_items, inverse_of: :orders
+
+  before_save :update_subtotal
+
+  def subtotal
+    order_items.collect do |item|
+      if item.valid?
+        (item.quantity * item.unit_price)
+      else
+        0
+      end
+    end.sum
+  end
+
+  private
+    def update_subtotal
+      self[:subtotal_amout] = subtotal
+    end
 end
